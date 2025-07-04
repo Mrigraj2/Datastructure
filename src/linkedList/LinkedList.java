d1aee98ef585dcea78a796705509305c498b949d
Mrigraj <20201746@sbsstc.ac.in>
2025-07-05 04:32:18 +0530

    Hello world type shit

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -1,4 +1,40 @@
 package linkedList;
 
 public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    int length;
+
+    class Node{
+        int value;
+        Node next;
+
+        public Node(int value){
+            this.value=value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "value=" + value +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+    public LinkedList(int value){
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+    }
+
+    @Override
+    public String toString() {
+        return "LinkedList{" +
+                "head=" + head +
+                ", tail=" + tail +
+                ", length=" + length +
+                '}';
+    }
 }
---
+++ src/linkedList/Main.java
@@ -0,0 +1,11 @@
+package linkedList;
+
+import linkedList.LinkedList;
+
+public class Main {
+    public static void main(String args[]){
+        LinkedList linkedList = new LinkedList(4);
+        System.out.println(linkedList.toString());
+    }
+
+}
