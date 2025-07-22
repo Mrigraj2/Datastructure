5f24de9ae19f40b1d8295531f8e1468fa30fe7dc
Mrigraj <20201746@sbsstc.ac.in>
2025-07-22 05:38:01 +0530

    Nodes creation

---
+++ src/linkedList/Pahade.java
@@ -0,0 +1,15 @@
+package linkedList;
+
+public class Pahade {
+
+    private Node pahade;
+    private LinkedList linkedListPahade;
+
+    public void pahadeNode(int value){
+        pahade.setValue(value);
+        linkedListPahade.head = pahade;
+        for (int i =2;i<=10;i++){
+
+        }
+    }
+}
